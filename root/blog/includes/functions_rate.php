cb4bed4849632f720b4860ad75a4e4e8be206c8d
exreaction <exreaction@a294dfe0-ea36-0410-9d4a-05498620a423>
2007-12-28 03:26:07 +0000

    Alpha 36
    git-svn-id: http://userblogmod.googlecode.com/svn/trunk@105 a294dfe0-ea36-0410-9d4a-05498620a423

--- root/blog/includes/functions_rate.php
+++ root/blog/includes/functions_rate.php
@@ -70,25 +70,17 @@ function get_star_rating($int_rating, $blog_id, $average = false)
 		return false;
 	}
 
-	$category_id = request_var('c', 0);
-	$final_code = ($average) ? $user->lang['AVERAGE_RATING'] . ': ' : '';
-
 	// We will need the blog_url function
 	if (!function_exists('blog_url'))
 	{
 		include("{$phpbb_root_path}blog/includes/functions_url.$phpEx");
 	}
 
+	// Make sure we have the language we need
 	if (!isset($user->lang['RATE_ME']))
 	{
 		$user->add_lang('mods/blog/view');
 	}
-
-	$star_green = $phpbb_root_path . 'styles/' . $user->theme['imageset_path'] . '/imageset/blog/star_green.gif';
-	$star_grey = $phpbb_root_path . 'styles/' . $user->theme['imageset_path'] . '/imageset/blog/star_grey.gif';
-	$star_orange = $phpbb_root_path . 'styles/' . $user->theme['imageset_path'] . '/imageset/blog/star_orange.gif';
-	$star_red = $phpbb_root_path . 'styles/' . $user->theme['imageset_path'] . '/imageset/blog/star_red.gif';
-	$star_remove = $phpbb_root_path . 'styles/' . $user->theme['imageset_path'] . '/imageset/blog/star_remove.gif';
 
 	// Get the rating data for this user
 	if ($user->data['is_registered'])
@@ -102,9 +94,24 @@ function get_star_rating($int_rating, $blog_id, $average = false)
 		$int_rating = ceil($config['user_blog_max_rating'] / 2);
 	}
 
+	// Some variables we'll need
+	$category_id = request_var('c', 0);
+	$star_green = $phpbb_root_path . 'styles/' . $user->theme['imageset_path'] . '/imageset/blog/star_green.gif';
+	$star_grey = $phpbb_root_path . 'styles/' . $user->theme['imageset_path'] . '/imageset/blog/star_grey.gif';
+	$star_orange = $phpbb_root_path . 'styles/' . $user->theme['imageset_path'] . '/imageset/blog/star_orange.gif';
+	$star_red = $phpbb_root_path . 'styles/' . $user->theme['imageset_path'] . '/imageset/blog/star_red.gif';
+	$star_remove = $phpbb_root_path . 'styles/' . $user->theme['imageset_path'] . '/imageset/blog/star_remove.gif';
+
+	$average = ($average || $user->data['user_id'] == $blog_data->blog[$blog_id]['user_id']) ? true : false;
+	$can_rate = ($user->data['is_registered'] && !$average && !isset($rating_data[$blog_id])) ? true : false;
+
+	$final_code = ($average) ? $user->lang['AVERAGE_RATING'] . ': ' : '';
+
+	// A unique string that will get added to the rating.  So if a blog is shown more than once, hovering over and trying to rate one doesn't mess up the other list.
 	$unique_str = md5(microtime());
 	$unique_str = "b_{$blog_id}_u_{$unique_str}_s_";
 
+	// If the user has rated this already, and we are not just getting the average, get the average as well.
 	if (isset($rating_data[$blog_id]) && !$average)
 	{
 		$final_code = get_star_rating($int_rating, $blog_id, true) . '';
@@ -116,7 +123,7 @@ function get_star_rating($int_rating, $blog_id, $average = false)
 	$final_code .= '<div>';
 	for ($i = $config['user_blog_min_rating']; $i <= $config['user_blog_max_rating']; $i++)
 	{
-		$final_code .= ($user->data['is_registered'] && !$average && !isset($rating_data[$blog_id])) ? '<a href="' . blog_url(false, $blog_id, false, array('page' => 'rate', 'rating' => $i, 'c' => (($category_id) ? $category_id : '*skip*'))) . '">' : '';
+		$final_code .= ($can_rate) ? '<a href="' . blog_url(false, $blog_id, false, array('page' => 'rate', 'rating' => $i, 'c' => (($category_id) ? $category_id : '*skip*'))) . '">' : '';
 		$final_code .= '<img id="' . $unique_str . $i . '" ';
 		if (isset($rating_data[$blog_id]) && $i <= $rating_data[$blog_id] && !$average)
 		{
@@ -130,9 +137,9 @@ function get_star_rating($int_rating, $blog_id, $average = false)
 		{
 			$final_code .= 'src="' . $star_grey . '" ';
 		}
-		$final_code .= ($user->data['is_registered'] && !$average && !isset($rating_data[$blog_id])) ? "onmouseover=\"ratingHover('{$i}', '{$unique_str}')\"  onmouseout=\"ratingUnHover('{$int_rating}', '{$unique_str}')\"  onmousedown=\"ratingDown('{$i}', '{$unique_str}')\"" : '';
+		$final_code .= ($can_rate) ? "onmouseover=\"ratingHover('{$i}', '{$unique_str}')\"  onmouseout=\"ratingUnHover('{$int_rating}', '{$unique_str}')\"  onmousedown=\"ratingDown('{$i}', '{$unique_str}')\"" : '';
 		$final_code .= ' alt="' . sprintf($user->lang['RATE_ME'], $i, $config['user_blog_max_rating']) . '" title="' . sprintf($user->lang['RATE_ME'], $i, $config['user_blog_max_rating']) . '" />';
-		$final_code .= ($user->data['is_registered'] && !$average && !isset($rating_data[$blog_id])) ? '</a>' : '';
+		$final_code .= ($can_rate) ? '</a>' : '';
 	}
 
 	// If required, we will add the remove rating icon at the end
